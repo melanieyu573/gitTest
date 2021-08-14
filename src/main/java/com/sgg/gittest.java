1277cd1f15de68d5d955259fe0f4ab9fd4c2baf5
yumei <melanieyu573@163.com>
2021-08-14 23:10:48 +0800

    test commit by idea

--- src/main/java/com/sgg/gittest.java
+++ src/main/java/com/sgg/gittest.java
@@ -5,7 +5,5 @@ public class gittest {
         System.out.println("nnnn");
         System.out.println("222");
         System.out.println("m3");
-        System.out.println("hotfix 1");
-        System.out.println("hotfix 22");
     }
 }
